2901206648502317a546bbb2941daf05936d7e77
huangjuntu <1721053474@qq.com>
2016-10-20 10:54:00 +0800

    initial

---
+++ js/app.switch.left.js
@@ -0,0 +1,84 @@
+		$(document).ready(function(){
+			var winhref = window.location.href;
+			var localIp = winhref.split('/');
+    
+				window.nodeIp = "http://"+ localIp[2] + ":10002";
+			var socket = io.connect(window.nodeIp);
+			socket.on('page', function(data){
+				console.log(data);
+				console.log(data.no);
+				switch(data.no)
+				{
+				case 0:
+					window.location = "http://127.0.0.1/html/left.html";
+					break;
+				case 1:
+					window.location = "http://127.0.0.1/test/1014/shkjw_left.html"; // 仪电云	
+				  	break;
+				case 2:
+					window.location = "http://127.0.0.1/test/1014/zhzz_left.html"; //智慧制造
+				  	break;
+				case 3:
+					window.location = "http://127.0.0.1/test/1014/zhld_left.html"; //智慧路灯
+				  	break;
+				case 4:
+					window.location = "http://127.0.0.1/test/1014/yyy_left.html"; //云应用
+				  	break;
+				case 5:
+					window.location = "http://127.0.0.1/test/1014/spsy_left.html"; //食品溯源
+				  	break;
+				case 6:
+					window.location = "http://127.0.0.1/test/1014/lxsh_left.html"; //智慧交通-乐行上海
+				  	break;
+				case 7:
+					window.location = "http://127.0.0.1/test/1014/gdjt_left.html"; //智慧交通-地铁
+				  	break;
+				case 8:
+					window.location = "http://127.0.0.1/test/1014/ld.html"; //智慧交通-雷达
+					break;
+				// case 9:
+				// 	window.location = "http://127.0.0.1/test/1014/ZhiHuiJiaoTong/ZHJT-left.html"; //智慧交通-雷达
+				//   	break;
+				case 9:
+					window.location = "http://127.0.0.1/test/1014/ydxs_left.html"; //智慧教育
+				  	break;
+				case 10:
+					window.location = "http://127.0.0.1/test/1014/zhsw_left.html"; //智慧水务
+				  	break;
+				case 11:
+					//window.location = "http://127.0.0.1/dataA"; //社区广播
+					window.location = "#"; //社区广播
+				  	break;
+				case 12:
+					window.location = "http://127.0.0.1/test/1014/zhsw2_left.html"; //智慧园区水质管理
+				  	break;
+				case 13:
+					window.location = "http://127.0.0.1/test/1014/13.html"; //智慧建筑
+				  	break;
+				case 14:
+					window.location = "http://127.0.0.1/test/1014/hxgf_left.html"; //商业不动产
+				  	break;
+				case 15:
+					window.location = "http://127.0.0.1/test/1014/hxzq_left.html"; //华鑫证券
+				  	break;
+				case 16:
+					window.location = "http://127.0.0.1/test/16.html"; //
+				  	break;
+				case 17:
+					window.location = "http://127.0.0.1/test/1014/17.html";
+				  	break;
+				case 18:
+					window.location = "http://127.0.0.1/test/1014/18.html";
+				  	break;
+				case 19:
+					window.location = "#";
+				  	break;
+				default:
+					window.location = "http://127.0.0.1/html/left.html";
+					break;
+				}
+			});
+		    
+        
+	 
+		});
---
+++ js/index.js
@@ -0,0 +1,130 @@
+$(function() {
+	$(".desk_left_bottom_one").click(function() {
+		$(".desk_left_bottom_one").removeClass("background1");
+		$(this).addClass("background1");
+		var herf = $(this).attr("src");
+		$(".iframe_fa").show();
+		$("#icon_iframw").attr("src", herf);
+		$(".icon").removeClass("white");
+		$(".img_icon").each(function() {
+			$(this).attr("src", $(this).attr("src1"));
+		})
+	})
+
+	$('a.icon').click(function(event) {//每个图标点击
+		event.preventDefault();
+		$(".desk_left_bottom_one").removeClass("background1");//两个icon图标 点击后加入背景色
+		
+		var herf = $(this).attr('href');//得到当前的href
+		$(".logo2").hide();
+		$(".iframe_fa").show();
+		$("#icon_iframw").attr("src", herf);
+		
+		
+		$(".icon").removeClass("white");
+		$(this).addClass("white");
+		$(".img_icon").each(function() {
+			$(this).attr("src", $(this).attr("src1"));
+		})
+		$(this).find("img").attr("src", $(this).find("img").attr("src2"));
+	});
+
+//新增加交通三项
+$('#leap5-1 .a,#leap5-1 .b,#leap5-1 .c').css("opacity","0");
+	$("#leap5").mouseover(function(event) {
+		//		event.preventDefault();
+		$("#leap5").css("display", "none");
+		$("#leap5-1").css("display", "block");
+		$(".copy5").css("display", "none");
+		setTimeout(function() {
+			$('#leap5-1 .a').css("opacity","1");
+			$('#leap5-1 .a').addClass("animated bounceInLeft");
+			setTimeout(function() {
+				$('#leap5-1 .b').css("opacity","1");
+				$('#leap5-1 .b').addClass("animated bounceInLeft");
+				setTimeout(function() {
+					$('#leap5-1 .c').css("opacity","1");
+					$('#leap5-1 .c').addClass("animated bounceInLeft");
+
+				}, 500);
+			}, 500);
+		}, 200);
+	});
+	$('#leap5-1').mouseleave(function() {
+		//		clearTimeout(t);
+		//		t = setTimeout(zoomIn, 400);
+		$("#leap5-1").css("display", "none");
+		$("#leap5").css("display", "block");
+	});
+//	新增加智慧园区两项
+
+	//新增加仪电云三项
+$('#leap1-1 .a,#leap1-1 .b,#leap1-1 .c').css("opacity","0");
+	$("#leap1").mouseover(function(event) {
+		//		event.preventDefault();
+		$("#leap1").css("display", "none");
+		$("#leap1-1").css("display", "block");
+		$(".copy1").css("display", "none");
+		setTimeout(function() {
+			$('#leap1-1 .a').css("opacity","1");
+			$('#leap1-1 .a').addClass("animated bounceInLeft");
+			setTimeout(function() {
+				$('#leap1-1 .b').css("opacity","1");
+				$('#leap1-1 .b').addClass("animated bounceInLeft");
+				setTimeout(function() {
+					$('#leap1-1 .c').css("opacity","1");
+					$('#leap1-1 .c').addClass("animated bounceInLeft");
+
+				}, 500);
+			}, 500);
+		}, 200);
+	});
+	$('#leap1-1').mouseleave(function() {
+		//		clearTimeout(t);
+		//		t = setTimeout(zoomIn, 400);
+		$("#leap1-1").css("display", "none");
+		$("#leap1").css("display", "block");
+	});
+	//	新增加智慧水务两项
+	$('#leap7-1 .a,#leap7-1 .b').css("opacity","0");
+	$("#leap7").mouseover(function(event) {
+		console.log("122");
+		//		event.preventDefault();
+		$("#leap7").css("display", "none");
+		$("#leap7-1").css("display", "block");
+		$(".copy7").css("display", "none");
+		setTimeout(function() {
+			$('#leap7-1 .a').css("opacity","1");
+			$('#leap7-1 .a').addClass("animated bounceInLeft");
+			setTimeout(function() {
+				$('#leap7-1 .b').css("opacity","1");
+				$('#leap7-1 .b').addClass("animated bounceInLeft");
+			}, 500);
+		}, 200);
+	});
+	$('#leap7-1').mouseleave(function() {
+		$("#leap7-1").css("display", "none");
+		$("#leap7").css("display", "block");
+	});
+	//	新增加智慧建筑两项
+	$('#leap8-1 .a,#leap8-1 .b').css("opacity","0");
+	$("#leap8").mouseover(function(event) {
+		console.log("122");
+		//		event.preventDefault();
+		$("#leap8").css("display", "none");
+		$("#leap8-1").css("display", "block");
+		$(".copy8").css("display", "none");
+		setTimeout(function() {
+			$('#leap8-1 .a').css("opacity","1");
+			$('#leap8-1 .a').addClass("animated bounceInLeft");
+			setTimeout(function() {
+				$('#leap8-1 .b').css("opacity","1");
+				$('#leap8-1 .b').addClass("animated bounceInLeft");
+			}, 500);
+		}, 200);
+	});
+	$('#leap8-1').mouseleave(function() {
+		$("#leap8-1").css("display", "none");
+		$("#leap8").css("display", "block");
+	});
+})
